83d11ec302c2178d8e779e82329db3f6b6bcdbe7
Robert Winkler <rwinkler@antmicro.com>
2020-09-08 17:19:00 +0200

    Update sphinx-verilog-domain version
    Signed-off-by: Robert Winkler <rwinkler@antmicro.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name="sphinx-verilog-domain",
-    version="0.0.1",
+    version="0.0.2",
     author="SymbiFlow Authors",
     author_email="symbiflow@lists.librecores.org",
     description="Verilog Domain for Sphinx",
