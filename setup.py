c0f54c534a651a1165cadc271b1c8d63de2d23b8
Tim 'mithro' Ansell <me@mith.ro>
2020-09-17 11:41:37 -0700

    setup: Small cleanup and use version.
    Signed-off-by: Tim 'mithro' Ansell <me@mith.ro>

--- setup.py
+++ setup.py
@@ -1,13 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020  The SymbiFlow Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import sys
+from os import path
+
 import setuptools
+
+__dir__ = path.dirname(path.abspath(__file__))
+
+# Manually import the version number so dependencies don't need to be installed
+# when running setup.py
+version_file = path.join(__dir__, "sphinx_verilog_domain", "version.py")
+if path.exists(version_file):
+    exec(open(version_file).read())
+else:
+    __version__ = "0.0.dev0"
 
 with open("README.rst", "r") as fh:
     long_description = fh.read()
 
+install_requires = [
+    'setuptools',
+    'docutils',
+    'sphinx',
+    'lark-parser'
+]
+
 setuptools.setup(
     name="sphinx-verilog-domain",
-    version="0.0.2",
+    version=__version__,
     author="SymbiFlow Authors",
     author_email="symbiflow@lists.librecores.org",
+    #license="Apache 2.0",
     description="Verilog Domain for Sphinx",
     long_description=long_description,
     long_description_content_type="text/x-rst",
@@ -16,12 +56,6 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     package_data={'': ["verilog.lark"]},
     include_package_data=True,
-    install_requires=[
-        'setuptools',
-        'docutils',
-        'sphinx',
-        'lark-parser'
-    ],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Framework :: Sphinx :: Extension',
@@ -30,5 +64,8 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         'Natural Language :: English',
         'Topic :: Documentation :: Sphinx',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Text Processing',
     ],
+    install_requires=install_requires,
 )
